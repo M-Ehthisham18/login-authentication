8720d8a59696aa08007fb2d02b40105c526024ff
M-Ehthisham18 <ehthishamulhaq18@gamil.com>
2024-10-20 17:52:46 +0530

    minor changes for login

--- auth_project/auth_app/urls.py
+++ auth_project/auth_app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/',views.register_page, name='register_page'),
     path('',views.login_page,name='login_page'),
+    path('register/',views.register_page, name='register_page'),
     path('dashborder/',views.dashborder,name='dashborder'),
     path('logout/',views.logout_page, name= 'logout')
 ]
